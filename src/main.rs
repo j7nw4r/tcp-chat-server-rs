979e184dba988baa23a74167ff6f7a1dff833dab
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2023-07-23 13:36:43 -0400

    More cleanup/prep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use anyhow::Context;
-use bytes::{Bytes, BytesMut};
+use anyhow::{bail, Context};
+use std::net::{IpAddr, Ipv4Addr};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{
         tcp::{OwnedReadHalf, OwnedWriteHalf},
         TcpStream,
     },
-    sync::broadcast::{Receiver, Sender},
+    sync::broadcast::{error::RecvError, Receiver, Sender},
 };
 use tracing::{error, info};
 
@@ -14,10 +14,10 @@ use tracing::{error, info};
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
 
-    let tcp_listerner = tokio::net::TcpListener::bind("127.0.0.1:23234")
+    let tcp_listerner = tokio::net::TcpListener::bind((IpAddr::V4(Ipv4Addr::UNSPECIFIED), 23234))
         .await
-        .context("could not create TcpListener on 127.0.0.1:23234")?;
-    let (sender, _) = tokio::sync::broadcast::channel::<String>(5);
+        .context("could not create TcpListener")?;
+    let (sender, _) = tokio::sync::broadcast::channel::<String>(2);
 
     loop {
         let (tcp_stream, addr) = tcp_listerner
@@ -31,27 +31,32 @@ async fn main() -> anyhow::Result<()> {
             match process_stream(tcp_stream, sender).await {
                 Ok(_) => (),
                 Err(e) => {
-                    info!("process_stream returned error: {}", e)
+                    error!("process_stream returned error: {}", e)
                 }
             }
         });
     }
 }
+
 async fn process_stream(stream: TcpStream, sender: Sender<String>) -> anyhow::Result<()> {
     let (tcp_reader, tcp_writer) = stream.into_split();
     let (broadcast_sender, broadcast_receiver) = (sender.clone(), sender.subscribe());
-    if let Err(e) = tokio::select! {
-        read_result = read_tcp_write_chat(tcp_reader, broadcast_sender) => {
-            info!("read_result returned");
-            read_result
+    match tokio::select! {
+        read_tcp_write_chat_result = read_tcp_write_chat(tcp_reader, broadcast_sender) => {
+            info!("read_tcp_write_chat_result returned");
+            read_tcp_write_chat_result
         },
-        write_result = read_chat_write_tcp( tcp_writer, broadcast_receiver) => {
-            info!("write_result returned");
-            write_result},
+        read_chat_write_tcp_result = read_chat_write_tcp( tcp_writer, broadcast_receiver) => {
+            info!("read_chat_write_tcp_result returned");
+            read_chat_write_tcp_result
+        },
     } {
-        error!("{}", e);
-    };
-    Ok(())
+        Ok(unit) => Ok(unit),
+        Err(e) => {
+            error!("{}", e);
+            Err(e)
+        }
+    }
 }
 
 async fn read_tcp_write_chat(
@@ -59,19 +64,17 @@ async fn read_tcp_write_chat(
     chat_sender: Sender<String>,
 ) -> anyhow::Result<()> {
     loop {
-        let mut buf = vec![0u8; 1024];
+        let mut buf: Vec<u8> = vec![0u8; 1024];
         tcp_reader
             .readable()
             .await
             .context("could not determine if tcp stream was readable")?;
 
-        let read_length = tcp_reader.read(&mut buf).await?;
-        buf.truncate(read_length);
-        let msg = String::from_utf8(buf.to_vec())?;
-        info!("read: {}", msg);
-        let send_result = chat_sender.send(msg);
-        if let Err(e) = send_result {
+        let _read_length = tcp_reader.read(&mut buf).await?;
+        let msg = String::from_utf8(buf)?;
+        if let Err(e) = chat_sender.send(msg) {
             error!("send_result : {}", e);
+            bail!(e);
         }
     }
 }
@@ -80,8 +83,16 @@ async fn read_chat_write_tcp(
     mut chat_receiver: Receiver<String>,
 ) -> anyhow::Result<()> {
     loop {
-        let msg = chat_receiver.recv().await?;
-        info!("sending: {}", msg);
+        let msg = match chat_receiver.recv().await {
+            Ok(msg) => msg,
+            Err(e) => {
+                if let RecvError::Lagged(num_of_skip) = e {
+                    info!("lagged. skipped {} messages", num_of_skip);
+                    continue;
+                }
+                bail!(e)
+            }
+        };
         tcp_writer
             .write_all(format!("received: {}", msg).as_bytes())
             .await?;
